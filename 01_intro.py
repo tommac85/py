af0dc0b1702b22b579f7aabeea368712b07bbdfc
tommac85 <123874160+tommac85@users.noreply.github.com>
2023-01-31 19:49:50 +0100

    Create 01_intro.py

---
+++ 01_intro.py
@@ -0,0 +1,190 @@
+print('Ucz się Pythona!')
+
+
+# Poniżej zdefiniowane jest przybliżenie liczby pi
+pi = 3.1415926535
+
+age = 20
+print('Mam '+str(age) + ' lat.')
+
+a='Python'
+b='3.8'
+print('Uczę się języka',a,'w wersji', b)
+
+price = 199.99
+print('Towar kosztuje', price)
+
+# deklaracja zmiennych
+cena=34.99
+waga=10
+print(f'Cena: {cena} PLN. Waga: {waga} kg')
+
+# Używając formatowania f-string wydrukuj przybliżenie liczby pi
+# do drugiego miejsca po przecinku
+pi = 3.1415926535
+print(f'Przybliżenie liczby pi: {round(pi,2)}')
+
+# Poniżej zdefiniowane jest przybliżenie liczby pi
+pi = 3.1415926535
+ 
+# Używając formatowania f-string wydrukuj przybliżenie liczby pi 
+# do drugiego miejsca po przecinku
+print(f'Przybliżenie liczby pi: {pi:.2f}')
+
+#Używając trzech funkcji print() (jedna linia - jedna funkcja) wydrukuj poniższy tekst:
+
+
+
+# ----------------------------------------
+# WERSJA: 1.0.1
+# ----------------------------------------
+
+
+# Wskazówka: Linie odgradzające składają się z 40 znaków '-' (myślnik).
+b='-'
+a=40*b
+wersja = '1.0.1'
+print(a)
+print(f'WERSJA: {wersja}')
+print(a)
+
+# Używając czterech funkcji print() (jedna linia - jedna funkcja) wydrukuj poniższy tekst:
+
+
+
+# ========================================
+# autor: jannowak@poczta.com
+# data: 01-01-2021
+# ========================================
+
+
+# Wskazówka: Linie odgradzające składają się z 40 znaków '='.
+
+print(40*'#')
+print('autor: jannowak@poczta.com')
+print('data: 01-01-2021')
+print(40*'#')
+
+# Używając funkcji print() oraz argumentu sep ustawionego na znak '#' wydrukuj poniższy tekst:
+
+
+
+# 'summer#time#holiday'
+print('summer','time', 'holiday',sep='#')
+
+# Ćwiczenie 1
+# Napisz program, który obliczy pole koła o promieniu 5 cm.
+
+# Przyjmij w przybliżeniu wartość liczby pi:
+
+
+
+# pi = 3.14
+
+
+# Oczekiwany wynik:
+
+
+
+# Pole koła wynosi: 78.5 cm2
+
+
+pi = 3.14
+r= 5
+pole = pi*r**2
+print(f'Pole koła wynosi: {pole:.3f} cm2')
+
+
+# Ćwiczenie 2
+# Napisz program, który wyznaczy wartość przyszłą kwoty 1000 PLN przy rocznej stopie procentowej 3%
+# , kapitalizacji rocznej oraz 5-letnim okresie inwestycji. Wynik zaokrąglij do pełnych groszy.
+
+
+
+# Oczekiwany wynik:
+
+
+
+# Wartość końcowa inwestycji wynosi: 1159.27 PLN
+
+kwota=1000
+rrso=0.03
+okres = [1,2,3,4,5]
+text='Wartość końcowa inwestycji wynosi: '
+wynik1 = kwota*rrso+kwota
+kwota =wynik1
+i=1
+for i in okres:
+    wynik1
+    kwota
+    i= i+1
+    print(f'{text}{round(kwota,2)} PLN')
+# wynik1 = kwota*rrso+kwota
+# kwota =wynik1
+# wynik1 = kwota*rrso+kwota
+# kwota =wynik1
+# wynik1 = kwota*rrso+kwota
+# kwota =wynik1
+# wynik1 = kwota*rrso+kwota
+# kwota =wynik1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
